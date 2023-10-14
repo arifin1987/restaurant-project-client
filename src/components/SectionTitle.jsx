36a5e5c6fc134f2d775e50bca01c8d335d748ef0
Shohidul Arifin <shohidularifin@gmail.com>
2023-10-14 00:27:03 -0700

    section title added

---
+++ src/components/SectionTitle.jsx
@@ -0,0 +1,16 @@
+const SectionTitle = ({subHeading,heading})=>{
+    return(
+        <div className="text-center text-2xl my-4">
+            <p className="text-yellow-400">--{subHeading}--</p>
+        <h1 className="uppercase">{heading}</h1>
+
+
+        </div>
+        
+
+    )
+
+
+}
+export default SectionTitle;
+    
--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -10,11 +10,18 @@ import slide2 from '../../../assets/home/slide2.jpg'
 import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
 import slide5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from '../../../components/sectionTitle';
 
 
 const Category = () => {
     return (
+      
         <div>
+          <SectionTitle
+      
+      subHeading={'From 10 am to 10 pm'}
+      heading={'Order Online'}
+      ></SectionTitle>
             <Swiper
         slidesPerView={4}
         spaceBetween={30}
