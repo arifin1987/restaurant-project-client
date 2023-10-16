9d219ca9e491262262130e8fd24c7903da0b8bd0
Shohidul Arifin <shohidularifin@gmail.com>
2023-10-15 23:47:32 -0700

    parallax and testimonial added

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -4,20 +4,20 @@ import './Featured.css'
 
 const Featured = () => {
     return (
-        <div className="featured-item">
+        <div className="featured-item bg-fixed pt-8 my-20">
             <SectionTitle
             subHeading={'checkout'}
             heading={'Featured'}
             
             ></SectionTitle>
-            <div className=" md:flex gap-8 m-4">
+            <div className=" md:flex gap-8 m-4 bg-slate-500 bg-opacity-40 justify-center items-center px-36 pt-12 pb-20">
                 <div>
                 <img  src={featured} alt="" />
                 </div>
             
             <div>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam quisquam reiciendis in numquam repudiandae atque quae doloribus qui aperiam nobis!</p>
-            <button className="btn btn-secondary">Order now</button>
+            <p className='text-white'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam quisquam reiciendis in numquam repudiandae atque quae doloribus qui aperiam nobis!</p>
+            <button className="btn btn-outline border-0 border-b-4">Order now</button>
 
             </div>
            
---
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,50 @@
+import SectionTitle from "../../../components/sectionTitle";
+
+import React, { useEffect, useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+
+
+// import required modules
+import { Navigation } from 'swiper/modules';
+
+
+const Testimonials = () => {
+    const[reviews, setReviews] = useState([]);
+    useEffect(()=>{
+        fetch('reviews.json')
+        .then(res=> res.json())
+        .then(data=> setReviews(data));
+    },[])
+    return (
+        <div className="my-4">
+            <SectionTitle
+            subHeading={'What our client say'}
+            heading={'Testimonials'}
+            ></SectionTitle>
+
+<Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+       
+        {
+            reviews.map(review=>  <SwiperSlide
+            key={review._id}
+            >
+                <div className="m-20"> 
+                <p>{review.details}</p>
+                <h3 className="text-2xl text-orange-300">{review.name}</h3>
+                </div>
+
+            </SwiperSlide>)
+        }
+      </Swiper>
+            
+        </div>
+    );
+};
+
+export default Testimonials;
